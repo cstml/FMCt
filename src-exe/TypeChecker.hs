1cad2cd9f5c9bcbba439733c288480385e6d8e41
cstml <cstmlcodes@gmail.com>
2021-06-04 07:36:54 +0100

    docs: figuring out types.

--- src-exe/TypeChecker.hs
+++ src-exe/TypeChecker.hs
@@ -41,3 +41,83 @@ machineRun x y  =  z :=> flipper z
 -- example typings
 t2 = "a" :-> (VT "a" "3") :-> (VT "a" "2") :-> (VT "b" "5") :-> (VT "a" "1")
 t1 = "a" :-> "b" :-> "c"
+
+{-
+--------------------
+Ex. 0: What types mean:
+
+x: a :-> b :-> c :=> d :-> e
+---
+x: a ∨ (b ∨ c)  |-  d ∧ e
+
+y: * :=> a
+---
+y: * |- a
+y: a
+
+g: a :=> *
+---
+g: a |- *
+
+h: a :=> b
+---
+g: a |- b
+
+--------------------
+Ex. 1.1: Composition
+x: (a :-> b :-> c :=> d :-> e)
+y: (e :-> d       :=> f)
+---
+x >>> y : ( a :-> b :-> c :=> f)
+
+Or:
+x: (a ∨ b ∨ c |-  d) |- e
+y: (e ∨ d     |-  f)
+---
+x >>> y : ((a ∨ b ∨ c |- d) |- e) >>> ((e ∨ d) |-  f)
+x >>> y : (((a ∨ b ∨ c |- d) |- (e |- e ) >>>  (d |-  f)
+        :   a ∨ b ∨ c |- d  ∧  d |-  f)
+        :   a ∨ b ∨ c |- f)
+
+Rewritten:
+x: !A :=> !B
+y: ?B :=> !C
+x >>> y : !A :=> !C
+
+--------------------
+Ex. 1.2: Composition
+x: (a :-> b :-> c :=> f :-> d :-> e)
+y: (e :-> d       :=> g)
+---
+x >>> y : ( a :-> b :-> c :=> f :-> g )
+
+Rewritten:
+x: !A :=> f :-> !B
+y: ?B :=> !C
+x >>> y : !A :=> f :-> !C
+
+--------------------
+Ex. 1.3: Composition
+x: (a :-> b :-> c :=> f :-> d :-> e)
+y: (d :-> f       :=> g)
+---
+x >>> y : ( a :-> b :-> c :-> d :-> f :=> f :-> d :-> e :-> g )
+
+Rewritten:
+x: !A :=> !B :-> f
+y: ?B :=> !C
+x >>> y : !A :-> ?B :=> !B :-> f :-> !C
+
+--------------------
+Ex. 2: Currying
+x: a :->  b :-> c |:=>| d :-> e
+x: a :=> (b :-> c |:=>| d :-> e
+x: a :=>  b :=> c |:=>| d :-> e
+x: a :->  b :=> c |:=>| d :-> e
+ a ∨ b ∨ c            |-  d ∧ e
+(a ∨ b ∨ c) ∧ d       |-  e
+((a ∨ b ∨ c) ∧ d) ∧ e |- *
+((a ∧ d) ∨ (b ∧ d) ∨ (c ∧ d)) ∧ e |- *
+(a ∧ d ∧ e) ∨ (b ∧ d ∧ e) ∨ (c ∧ d ∧ e) |- *
+---
+-}
