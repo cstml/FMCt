575807b56828f336fb62d9975511189b4d5a87ed
cstml <cstmlcodes@gmail.com>
2021-06-08 17:03:44 +0100

    update: evaluator can do + operation and a first version of EvalIO

--- src-exe/Evaluator.hs
+++ src-exe/Evaluator.hs
@@ -9,33 +9,69 @@ module Evaluator
 import Syntax
 import Data.Map (Map, (!?))
 import qualified Data.Map as M
+import Data.Char
+import Data.String (IsString(..))
+import Text.Read
 
-type Term = Tm
+type Term   = Tm
 type Memory = Map Lo [Term]
 type Binds  = Map Vv [Term]
 type State  = (Memory, Binds)
-
+  
+-- | Pushes a term to a location the memory
 push :: Term -> Lo -> State -> State
-push t In  _ = error $ "Cannot push term: " ++ show t ++ " to in"
-push t Rnd _ = error $ "Cannot push term: " ++ show t ++ " to rnd"
-push t Nd  _ = error $ "Cannot push term: " ++ show t ++ " to nd"
-push t l (m,b) = case (m !? l) of
+push t In  _ = error $ "Cannot push term: " ++ show t ++ " to in"   -- not allowed
+push t Rnd _ = error $ "Cannot push term: " ++ show t ++ " to rnd"  -- not allowed
+push t Nd  _ = error $ "Cannot push term: " ++ show t ++ " to nd"   -- not allowed
+push t l (m,b) = case m !? l of
                Nothing -> (M.insert l [t]   m, b)
                Just x  -> (M.insert l (t:x) m, b)
 
+-- | Pops a term from λ location to a given location
+pushL :: Lo -> State -> State
+pushL l st@(m,b) = case m !? Ho of
+                     Nothing     -> error "Empty Stack when attempting to pop λ location"                    
+                     Just (x:xs) -> push x l (M.insert Ho xs m, b)
+                     Just _      -> error "Empty Stack when attempting to pop λ location"
+
+-- | Binds a term to a value and puts in the memory
 bind :: Vv -> Lo -> State -> State
-bind vv l st@(m,b) = case (m !? l) of
+bind vv l st@(m,b) = case m !? l of
                    Nothing      -> error $ "Empty Location " ++ show l
                    Just []      -> error $ "Empty Location " ++ show l
                    Just (x:xs)  -> (M.insert l xs m, M.insert vv [x] b)
 
-emptyMem :: State
-emptyMem = (M.empty, M.empty)
+-- | Pops one value from a given location
+pop :: Int -> Lo -> State -> State
+pop n l st@(m,b)
+  | n < 1     = st
+  | otherwise = pop (n - 1) l nSt
+    where
+      nSt = case m !? l of
+              Just []     -> error "Empty Stack"
+              Nothing     -> error "Empty Stack"
+              Just (x:xs) -> push x Ho (M.insert l xs m, b)
 
+-- | Hacky way to add numbers - TODO: refactor
+adds :: String -> String -> String
+adds x y = case ((readMaybe x) :: Maybe Int, (readMaybe y) :: Maybe Int) of
+             (Just x, Just y) -> show $ x + y
+             _                -> x ++ y
+
+add :: State -> State
+add st@(m,b) = t
+  where
+    t = case m !? Ho of
+      Just []       -> error "Empty Stack"
+      Just (x:y:xs) -> case (x, y) of
+                         (Va x _ , Va y _) -> push (Va (x `adds` y) St) Ho (M.insert Ho xs m, b)
+        
 evaluate :: Term -> State -> State
-
-evaluate St       m = m              -- does nothing
-
+evaluate (Va "+" c) m = evaluate c $ add (pop 2 Ho m)
+  where
+    intermediary = add $ pop 2 Ho m
+    
+evaluate St         m = m                  -- does nothing
 evaluate (Va x c) st@(m,b) = evaluate c nt -- places value @ lambda pos
   where
     nt = if unbound then push (Va x St) Ho st
@@ -45,21 +81,27 @@ evaluate (Va x c) st@(m,b) = evaluate c nt -- places value @ lambda pos
            _      -> error "This should have never happened - pushing unbound"
     unbound = case b !? x of
                 Nothing -> True
-                _       -> False
-                
+                _       -> False                
 evaluate (Ab v ty lo tm) m = evaluate tm (bind v lo m)            -- pops and binds the term
-
 evaluate (Ap (Va x c) l t') st@(m,b) = evaluate t' (push te l st) -- pushes the bound term
   where
     te = case b !? x of
       Nothing -> St
-      Just x  -> head x
-      
+      Just x  -> head x      
 evaluate (Ap te l t') st = evaluate t' (push te l st)       -- pushes the term as is
+              
+emptyMem :: State
+emptyMem = (M.empty, M.empty)
 
 -- | Takes a list of terms and evaluates them
 eval :: [Term] -> State
 eval t = foldl1 (flip (.)) (evaluate <$> t) emptyMem
+
+evalIO :: State -> IO ()
+evalIO st@(m,b) = case m !? Out of
+                    Just (x:xs)  -> putStrLn (show x) >> evalIO (M.insert Out xs m, b)
+                    Just []      -> return ()
+                    Nothing      -> return ()
 
 ex1 = evaluate St emptyMem
 
@@ -87,3 +129,5 @@ ex5 = eval [t3, t2, t5, t6]
     t4 = (Ab "z" "int" Ho St)
     t5 = (Va "c" St)
     t6 = (Ap "x" "out" St)
+
+ex6 = evalIO $ eval [Va "2" $ Va "232132" $ Va "+" $ Ab "out" "Int" Ho $ Ap "out" "out" $ St ]
