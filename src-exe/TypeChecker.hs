b05cdfe488f68cd393093fdc2f093a6dd4ea6d0f
cstml <cstmlcodes@gmail.com>
2021-06-16 17:09:34 +0100

    update: simple Fusion working

--- src-exe/TypeChecker.hs
+++ src-exe/TypeChecker.hs
@@ -38,6 +38,14 @@ mCL (L m) (L m') = L $ M.unionWith (<>) m m'
 mDL :: L -> L -> L
 mDL (L m) (L m') = L $ M.unionWith (>\) m m'
 
+-- | Fusion is the type equivalent of the . or ; of the term.
+(>>>) :: TT -> TT -> TT
+(WT a :=> WT b) >>> (WT c :=> WT d) = tI :=> tO
+  where
+    tI = WT $ mCL a (mDL c b)
+    tO = WT $ mCL d (mDL b d)
+
+
 -- Examples:
 ex1 = T ( C "a") <> T ( C "a") <> mempty <> T ( C "a") -- example simple type
 ex1' = T ( C "a") <> T ( C "b") <> mempty <> T ( C "c") -- example simple type
@@ -48,68 +56,40 @@ ex5 = mDL ex4 ex4 -- merging location parametrised types
 ex6 = ((WT ex4) :=> (WT ex4)) == ((WT ex4) :=> (WT (sL Ho $ T E))) -- comparisson of types works
 ex7 = let a = ((WT ex4) :=> (WT ex4)) in a == a 
 
+--------------------------------------------------------------------------------
+-- Examples fusion
+exf1 = t1 :=> t2
+  where
+    t1 = WT $ sL Ho (T E)
+    t2 = WT $ sL Ho (T E)
+
+exf2 = t1 :=> t2
+  where
+    t1 = WT $ sL Ho (T E)
+    t2 = WT $ sL Ho (T $ C "Int")
+
+exf3 = exf1 >>> exf2
 
 {-
 --------------------
-Ex. 0: What types mean:
-
-x: a :-> b :-> c :=> d :-> e
+Ex. 1.1: Fusion
+x: a b c => d e
+y: d e => f
 ---
-x: a  ∧ (b ∧ c)  |-  d ∧ e
-
-y: * :=> a
----
-y: * |- a
-y: a
-
-g: a :=> *
----
-g: a |- *
-
-h: a :=> b
----
-g: a |- b
+x >>> y : a b c => f
 
 --------------------
-Ex. 1.1: Composition
-x: (a :-> b :-> c :=> d :-> e)
-y: (e :-> d       :=> f)
+Ex. 1.2: Fusion
+x: a b c => d e g
+y: d e => f
 ---
-x >>> y : ( a :-> b :-> c :=> f)
-
-Or:
-x: a ∨ b ∨ c |-  d ∧ e
-y: e ∨ d     |-  f
----
-x >>> y : a ∨ b ∨ c |- f)
-
-Rewritten:
-x: !A :=> !B
-y: ?B :=> !C
-x >>> y : !A :=> !C
+x >>> y : a b c => f g 
 
 --------------------
-Ex. 1.2: Composition
-x: (a :-> b :-> c :=> f :-> d :-> e)
-y: (e :-> d       :=> g)
+Ex. 1.3: Fusion
+x: a b c => d e g
+y: x k y => z h
 ---
-x >>> y : ( a :-> b :-> c :=> f :-> g )
-
-Rewritten:
-x: !A :=> f :-> !B
-y: ?B :=> !C
-x >>> y : !A :=> f :-> !C
-
---------------------
-Ex. 1.3: Composition
-x: (a :-> b :-> c :=> f :-> d :-> e)
-y: (d :-> f       :=> g)
----
-x >>> y : ( a :-> b :-> c :-> d :-> f :=> f :-> d :-> e :-> g )
-
-Rewritten:
-x: !A :=> !B :-> f
-y: ?B :=> !C
-x >>> y : !A :-> ?B :=> !B :-> f :-> !C
+x >>> y : a b c x k y => z h d e g
 
 -}
