2afdacf99520f410669401a61bfc27033d26aed4
cstml <cstmlcodes@gmail.com>
2021-06-18 12:30:08 +0100

    update: very in flux

--- src-exe/TypeChecker.hs
+++ src-exe/TypeChecker.hs
@@ -9,13 +9,8 @@ import Data.Monoid
 import qualified Data.Map  as M
 import           Data.Map  (Map, (!?))
 
-instance Semigroup T where
-  (T E) <> t  = t
-  t <> (T E)  = t 
-  t1@(T t) <> t2@(T t') = TV $ t1 : t2 :[]
-  t1@(T t) <> t2@(TV t') = TV $ t1 : t'
-  t1@(TV t) <> t2@(T t') = TV $ t ++ [t2]
-  t1@(TV t) <> t2@(TV t') = TV $ t ++ t'
+instance Semigroup (VT a) where
+  VT a <> VT b = VT $ a ++ b
 
 instance Monoid T where
   mempty = T E
