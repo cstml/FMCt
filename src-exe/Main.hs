4d3d38241ae047b6d3d4a8c79af7682aa2600f4f
cstml <cstmlcodes@gmail.com>
2021-06-23 15:51:53 +0100

    working:web

--- src-exe/Main.hs
+++ src-exe/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 import Syntax
 import Parsing
@@ -7,13 +9,43 @@ import Examples
 import TypeChecker
 import Control.Monad
 import Data.String (IsString(..))
+import qualified System.Environment as Sys
+import Web.Scotty
+import qualified Data.Text.Lazy as L
+import qualified Data.Text as T
+import Data.Text.Lazy(Text)
 
-main :: IO ()
-main =
+herokuGetPort :: IO Int
+herokuGetPort = do
+  args <- (Sys.getEnv "PORT")
+  port <- (return . read) args::IO Int
+  return port
+
+main = do port <- herokuGetPort
+          scotty port $ mconcat [a]
+            where
+              a = get "/parse/:term" $ do
+                term <- param "term"
+                html $ mconcat ["<h1>", compute term , "</h1>"]
+
+compute :: Text -> Text
+compute x = ( L.fromStrict
+            . T.pack
+            . run
+            . show
+            . L.toStrict ) x
+
+run :: String -> String
+run = (show . eval1 . parseFMC . reverse . tail . reverse . tail)
+
+main' :: IO ()
+main' = 
   let
-    break = (take 80 . repeat) '=' 
+    break = (take 80 . repeat) '='
   in 
-  do putStrLn $
+  do port <- herokuGetPort
+     print port
+     putStrLn $
        break ++ "\n"
        ++ "Hello, and welcome to the FMCt REPL \n"
        ++ "May the λ be with you!\n"
