14f70ae8558ce5dbb12090d05576bd9d3e15156d
cstml <cstmlcodes@gmail.com>
2021-07-21 16:14:05 +0100

    update: surfacing Pretty

--- src/FMCt.hs
+++ src/FMCt.hs
@@ -1,7 +1,8 @@
-module FMCt where
-
+module FMCt
+  (  module FMCt  )
+where
 -- Module is made just for Re-Exporting basic functions.
-import FMCt.Parsing    ( parseFMC, parseType, parseFMCtoString)
-import FMCt.Evaluator  ( eval )
-import FMCt.Examples   ( examplesList )
-import FMCt.TypeChecker( typeCheck )
+import FMCt.Parsing     ( parseFMC, parseType, parseFMCtoString )
+import FMCt.Evaluator   ( eval )
+import FMCt.Examples    ( examplesList )
+import FMCt.TypeChecker ( typeCheck )
