4065688b73124ea0b0e1f66a1c77d956762d1911
cstml <cstmlcodes@gmail.com>
2021-07-21 12:11:05 +0100

    update: working

--- src/FMCt/Syntax.hs
+++ src/FMCt/Syntax.hs
@@ -4,13 +4,10 @@ Description : Syntax module of the FMCt.
 
 Syntax module of the FMCt.
 -}
+{-#LANGUAGE FlexibleInstances#-}
 module FMCt.Syntax
-  ( LTConstant(..)
-  , Lo(..)
+  ( Lo(..)
   , T
-  , TConstant
-  , TType
-  , TVariable
   , Tm(..)
   , Type(..)
   , Vv
@@ -28,7 +25,6 @@ type Vv = String            -- ^ Variable Value is represeted by a String.
 data Tm = V Vv Tm          -- ^ Variable
         | P Tm Lo Tm       -- ^ Application or Push: [M]a.N
         | B Vv T  Lo Tm    -- ^ Abstraction or Pop:  a\<x:t\>.N
---        | E Tm Tm          -- ^ Evaluate Term -- might drop
         | St               -- ^ Star 
         deriving (Eq, Ord)
 
@@ -38,29 +34,18 @@ data Tm = V Vv Tm          -- ^ Variable
 
 infixr 9 :=>
 
--- | Type Constants
-type TConstant = String -- ^ Type constants are strings, with void being represented by `""` or empty String.
-
--- | Type Variables
-type TVariable = String  -- ^ Type variables are placeholders that can be replaced with any type.
-
--- | Location Type Constant
-data LTConstant = T Lo TConstant
-        deriving (Eq, Ord)
-
-type T = Type LTConstant
+type T = Type String
 
 -- | Type data structure
-data Type a = TConst [a]         -- ^ Type Vector.
-            | TLocat Lo (Type a) -- ^ Location Parametrised Type.
+data Type a = TCon a           -- ^ Type Vector.
+            | TVar a
+            | TVec [Type a]
+            | TLoc Lo (Type a) -- ^ Location Parametrised Type.
             | Type a :=> Type a  -- ^ A FMC Type.
             deriving (Eq, Ord)
 
--- | Term Types hold either a Type Variable or a Location Type Constant
-type TType = Type (Either TVariable LTConstant)
-
 -- | TypedTerms
-type TTm = (Tm, TType)
+type TTm = (Tm, T)
 
 --------------------------------------------
 -- Location = {out, in, rnd, nd, x, γ, λ} --
@@ -91,10 +76,12 @@ instance Show Lo where
     La   -> "λ"
     Lo y -> y
 
-instance (Show a) => Show (Type a) where
+instance Show (Type String)  where
   show x = case x of
-    TConst y -> show y
-    TLocat l y -> show l ++ "(" ++ show y ++ ")"
+    TCon "" -> "*"
+    TCon y -> y
+    TVec x -> init $ mconcat $ (flip (++) ",") <$> show <$> x
+    TLoc l y -> show l ++ "(" ++ show y ++ ")"
     t1 :=> t2 -> mconcat ["(", show t1, " => ", show t2, ")"]
     --TVector x -> "(" ++ (mconcat $ (++ ", ") . show <$> x) ++ ")"
 
@@ -110,10 +97,8 @@ instance Show Tm where
 instance Functor Type where
   fmap f term
     = case term of
-        TConst x -> TConst $ f <$> x
-        TLocat l x -> TLocat l $ f <$> x
-        a :=> b -> (f <$> a) :=> (f <$> b)
-
-instance Show LTConstant where
-  show (T l c) = mconcat [show l, "(", c, ")"]
+        TCon x   -> TCon $ f x
+        TVar x   -> TVar $ f x
+        TLoc l x -> TLoc l $ f <$> x
+        a :=> b  -> (f <$> a) :=> (f <$> b)
                   
