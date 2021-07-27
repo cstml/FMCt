c854a155e76c919a61ae7a2d86a71dee4a781ac8
cstml <cstmlcodes@gmail.com>
2021-07-27 14:07:08 +0100

    update: working fuser

--- src/FMCt/TypeChecker.hs
+++ src/FMCt/TypeChecker.hs
@@ -1,19 +1,19 @@
+{-#LANGUAGE TypeSynonymInstances#-}
+{-#LANGUAGE FlexibleInstances#-}
+{-#LANGUAGE FlexibleContexts#-}
 module FMCt.TypeChecker
   ( typeCheck
   , TypeError(..)
   )
 where
 
-import Control.Monad.Trans.State
 import FMCt.Syntax
 import Data.List (sort)
-import Data.Semigroup
-import Data.Map (Map)
 import qualified Data.Map as M
-import Control.Monad.ST
-import FMCt.Parsing
+import FMCt.Parsing (parseType, parseFMC)
 import Text.Read (readMaybe)
-import Control.Applicative
+import Data.Monoid
+import Control.Monad.State
 
 -- | TypeChecking Error.
 data TypeError
@@ -507,6 +507,125 @@ ex63 = derive3 (parseFMC "[<x:(=>a)>.x.*].<y:a>.*")
 ex73 = derive3 (parseFMC "[*].*")
 ex83 = derive3 (parseFMC "[*].<x:(=>)>.*")
 
+instance Semigroup T where
+  TVec []       <> x              = x
+  x             <> TVec[]         = x
+  TCon ""       <> x              = x
+  x             <> TCon ""        = x
+  xx@(TCon x)   <> yy@(TCon y)    = TVec [xx,yy]
+  TVec x        <> TVec y         = TVec $ x ++ y
+  xx@(TLoc l x) <> yy@(TLoc l' y) | l == l' = TLoc l $ x <> y
+                                  | otherwise = TVec [xx,yy]
+  xx <> yy                        = TVec [xx,yy]
+
+instance Monoid T where
+  mempty = TCon ""
+
+fuseTypesD :: Derivation -> Derivation -> Maybe T
+fuseTypesD dL dR = ty 
+  where
+    tL = (getJType . getJudgement) dL :: T
+    tR = (getJType . getJudgement) dR :: T
+    ty = Just $ fuse tL tR
+
+fuse :: T -> T -> T
+fuse = \case
+  TCon ""      -> id
+  xx@(TCon x)  -> \case
+    yy@(TCon y)    -> TVec $ xx : yy : []
+    yy@(TVec y)    -> TVec $ xx : y
+    yy@(TLoc l y)  -> TVec $ xx : yy : []
+    yy@(t1 :=> t2) -> fuse (TCon "" :=> xx) yy
+
+  xx@(TVec x) -> \case
+    yy@(TCon y)    -> TVec $ x ++ [yy]
+    yy@(TVec y)    -> TVec $ x ++ y
+    yy@(TLoc l y)  -> TVec $ x ++ [yy]
+    yy@(t1 :=> t2) -> fuse (TCon "" :=> xx) yy
+
+  xx@(TLoc lx x) -> \case
+    yy@(TCon y)    -> TVec $ xx : yy : []
+    yy@(TVec y)    -> TVec $ [xx] ++ y
+    yy@(TLoc l y)  -> TVec $ [xx] ++ [yy]
+    yy@(t1 :=> t2) -> fuse (TCon "" :=> xx) yy
+
+  xx@(xTi :=> xTo) -> \case
+    yy@(yTi :=> yTo) -> if consumes yTi xTo 
+                         then  fuse xTi (consume  yTi xTo) :=> yTo                       
+                         else  error "Cannot fuse"
+                    
+consumes :: T -> T -> Bool
+consumes xx@(TCon x)    yy@(TCon y)   = xx == yy
+consumes xx@(t1 :=> t2) yy@(TCon y)   = consumes t1 yy 
+consumes xx             yy@(TVec [])  = True
+consumes (TVec [])      _             = False
+
+consume :: T -> T -> T
+consume = (<->)
+
+newtype MT = MT T
+
+pack :: T -> MT
+pack x@(t1 :=> t2) = MT x
+pack _             = error "Not Machine Type"
+
+
+
+-- | Fully Consumes one Another
+(<<->>) :: T -> T -> Maybe T
+TCon x <<->> TCon y
+  | x == y    = Just mempty
+  | otherwise = Nothing
+TVec x <<->> TVec y
+  | x == y    = Just mempty
+  | otherwise = Nothing
+TLoc l t <<->> TLoc l' t'
+  | l == l' && t == t' = Just mempty
+  | otherwise = Nothing
+(t1 :=> t2) <<->> (t1' :=> t2')
+  | t2 == t1' = Just $ t1 :=> t2'
+  | otherwise = Nothing
+
+(<->) :: T -> T -> T
+TCon x  <-> TCon y
+  | x == y       = mempty
+  | otherwise    = TCon x
+TVec []  <-> yy  = mempty
+TCon ""  <-> yy  = mempty
+TLoc l t <-> TLoc l' t'
+  | l == l'      = if t == t'
+                  then mempty
+                  else error "Different Location Types"
+  |otherwise     =  TLoc l t
+TVec(x:xs) <-> y =  x <-> y <> ((TVec xs) <-> (x <-> y))
+(t1 :=> t2)  <-> y = if t1 <-> y == mempty
+                     then mempty :=> t2
+                     else error $ "did not consume everything"
+x <-> TVec (y:ys) = x <-> y <> ((x<->y) <-> TVec ys)
+x <-> TVec []     = x
+x <-> TCon ""     = x
+
+                               
+
+cEx1 = TCon "a" <-> TCon "a"
+cEx2 = TCon "a" <-> TVec [TCon "a", TCon "a"]
+
+cEx3 = TVec [TCon "a", TCon "a"] <-> TVec [TCon "a", TCon "a", TCon "b"]
+cEx4 = TVec [TCon "a", TCon "a", TCon "b"] <-> TVec [TCon "a", TCon "a"]
+
+cEx5 = TVec[TCon "b"] <-> TVec [TCon "a", TCon "a"]
+
+cEx6 = TVec[TCon "a", TCon "a"] <<->> TVec [TCon "a", TCon "a"]
+
+typeToConsume :: T -> State T ()
+typeToConsume t = void $ put t
+
+consumeT :: T -> State T ()
+consumeT t = do
+  state <- get
+  put (state <-> t)
+                                                                           
+getJType (_,_,x) = x
 
 -- | Second step is to add our known variables to the context
 derive4 :: Term -> Derivation
@@ -518,11 +637,11 @@ derive4 = derive4' freshVarTypes [(St, TCon [] :=> TCon [])]
     -- | Get the context 
     gCtx :: Derivation -> Context
     gCtx = \case
-      Star (c,_,_) -> c
-      Variable (c,_,_) -> c
-      Abstraction (c,_,_) _ -> c
-      Application (c,_,_) _ -> c
-      Fusion (c,_,_) _ _ -> c
+      Star        (c,_,_)     -> c
+      Variable    (c,_,_)     -> c
+      Abstraction (c,_,_) _   -> c
+      Application (c,_,_) _   -> c
+      Fusion      (c,_,_) _ _ -> c
     
     getType :: Term -> Context -> T
     getType t [] = error $ "Cannot Find type for term: " ++ show t
@@ -547,8 +666,8 @@ derive4 = derive4' freshVarTypes [(St, TCon [] :=> TCon [])]
       Variable    (_,_,t)     -> t 
       Abstraction (_,_,t) _   -> t
       Application (_,_,t) _   -> t
-      Fusion      (_,_,t) _ _ -> t
-
+      Fusion      (_,_,t) _ _ -> t        
+    
     derive4' :: [T] -> Context -> Term -> Derivation
     derive4' stream ctx term 
       = case term of  
@@ -570,18 +689,18 @@ derive4 = derive4' freshVarTypes [(St, TCon [] :=> TCon [])]
               
           B x t lo St -> Abstraction (ctx', term, ty) nDeriv
             where              
-              ty = TLoc lo t :=> TCon []
+              ty      = TLoc lo t :=> TCon []
               nStream = tail stream
-              nDeriv = derive4' nStream ctx' (V x St)
-              ctx' = (V x St, t) : ctx 
+              nDeriv  = derive4' nStream ctx' (V x St)
+              ctx'    = (V x St, t) : ctx 
               
           B x t lo t' -> Fusion (ctx', term, ty) dLeft dRight
             where
-              dLeft = derive4' nStreamL ctx (B x t lo St)
-              dRight = derive4' nStreamR ctx' t'
-              ctx' = (V x St, t) : ctx
-              ty = head stream
-              nStream = tail stream
+              dLeft    = derive4' nStreamL ctx (B x t lo St)
+              dRight   = derive4' nStreamR ctx' t'
+              ctx'     = (V x St, t) : ctx
+              ty       = maybe (error "Bad Fusion") id $ fuseTypesD dLeft dRight
+              nStream  = tail stream
               (nStreamL, nStreamR) = splitStream nStream
 
           P t lo St -> Application (ctx, term, ty) nDeriv
@@ -599,6 +718,19 @@ derive4 = derive4' freshVarTypes [(St, TCon [] :=> TCon [])]
               nStream = tail stream
               (nStreamL, nStreamR) = splitStream nStream
               nctx =  mergeCtx (gCtx dLeft) (gCtx dRight)              
+
+      
+ex04 = derive4 $ parseFMC "*"
+ex14 = derive4 $ parseFMC "x.y.*"
+ex24 = derive4 $ parseFMC "<x:(=>a)>.*"
+ex34 = derive4 $ parseFMC "<x:a>.x.*"
+ex44 = derive4 $ parseFMC "<x:a>.<y:a>.x.*"
+ex54 = derive4 $ parseFMC "[x.*].*"
+ex64 = derive4 $ parseFMC "[<x:a>.x.*].*"
+ex74 = derive4 $ parseFMC "[<x:(=>a)>.x.*].<y:a>.*"
+ex84 = derive4 $ parseFMC "[*].*"
+ex94 = derive4 $ parseFMC "[*].<x:(=>)>.*"
+
 
 {-
 x.y.z
@@ -627,3 +759,166 @@ x.*     [*].[*].y.*
 x.[*].[*].y.*
 
 -}
+
+fuseN :: T -> T -> T
+fuseN = \case
+  x@(TCon "") -> id
+  x@(TVec []) -> id
+  
+  x@(TCon _)  -> \case
+    y@(TCon "")      ->  x
+    y@(TCon _)       -> TVec [x,y]
+    y@(TVec [])      -> TVec [x]
+    yy@(TVec (y:ys)) -> fuseN (fuseN x y) (TVec ys)
+    y@(TLoc l t)     -> TVec [x,y]
+    y@(t1 :=> t2)    -> either error id $ consumeN y (mempty :=> x)
+    
+  xxx@(TVec xx@(x:xs))  -> \case
+    y@(TCon "")      -> xxx
+    y@(TCon _)       -> TVec $ xx ++ [y]
+    y@(TVec [])      -> xxx
+    yy@(TVec (y:ys)) -> fuseN (fuseN xxx y) (TVec ys)
+    y@(TLoc l t)     -> TVec [x,y]
+    y@(t1 :=> t2)    -> either error id $ consumeN y (mempty :=> xxx)
+    
+  x@(TLoc l t)  -> \case
+    y@(TCon "")      -> x
+    y@(TCon _)       -> TVec $ x : y : []
+    y@(TVec [])      -> x
+    yy@(TVec (y:ys)) -> fuseN (fuseN x y) (TVec ys)
+    y@(TLoc l' t')   -> if l == l'
+                        then TLoc l $ fuseN t t'
+                        else TVec [x,y]
+    y@(t1 :=> t2)    -> either error id $ consumeN y (mempty :=> x)
+
+  x@(t1 :=> t2)  -> \case
+    y@(TCon "")      -> x
+    y@(TCon _)       -> fuseN (mempty :=> y) x
+    y@(TVec [])      -> x
+    yy@(TVec (y:ys)) -> fuseN (fuseN x y) (TVec ys)
+    y@(TLoc l' t')   -> fuseN (mempty :=> y) x
+    y@(t1' :=> t2')    -> either error id $ consumeN y x
+
+consumesN :: T
+          -- ^ Requires - What the term will consume
+          -> T
+          -- ^ Receives - what the term is getting 
+          -> (T,T)
+          -- ^ (Left from Requires, Left from Receives)
+consumesN = \case
+  TCon "" -> (,) mempty . id
+
+  TVec [] -> (,) mempty . id
+
+  xx@(TCon x)  -> \case
+    TCon "" -> (,) xx mempty
+    yy@(TCon y) -> if x == y then (,) mempty mempty
+                   else error $
+                        mconcat [ "cannot merge ", show xx , " " , show yy ]
+    TVec [] -> (,) xx mempty
+    TVec (y:ys) -> let
+        (res1, left1) = consumesN xx y
+        (res2, left2) = consumesN res1 (TVec ys)
+      in
+        (,) res2 (left1 <> left2)        
+    y -> (xx,y)
+
+  xxx@(TVec xx@(x:xs)) -> \case
+    TCon "" -> (,) xxx mempty
+    TVec [] -> (,) xxx mempty
+    yyy@(TVec yy@(y:ys)) -> let
+        (res1,left1) = consumesN xxx y
+        (res2,left2) = consumesN res1 (TVec ys)
+      in
+        (,) res2 (left1 <> left2)
+    y -> let
+        (res1,left1) = consumesN x y
+        (res2,left2) = consumesN (TVec xs) left1
+      in
+        (,) (res1 <> res2) (left2)
+
+  x@(TLoc l t) -> \case
+    TCon "" -> (,) x mempty
+    TVec [] -> (,) x mempty
+    y@(TLoc l' t') ->
+      -- ^ If the location are the same we need to check what happens with the
+      -- interaction between the types.
+      if l == l'
+      then
+        let              
+          (res, left) = consumesN t t'
+                        
+          res' = if res == mempty || res == TVec []
+                 then mempty
+                 else TLoc l res
+                     
+          left' = if left == mempty || left == TVec []
+                  then mempty
+                  else TLoc l left
+        in
+          (,) res left'
+
+        -- | Otherwise they don't interact 
+      else (,) x y
+
+    -- | TLoc doesn't interact with any other type.
+    y       -> (,) x y
+
+  x@(tIn :=> tOut) ->
+    \y ->
+      if x == y
+      then (,) mempty mempty
+      else (,) x y 
+      
+xc1 = consumesN (TCon "x") (TCon "x")
+xc2 = consumesN (TVec [ TCon "y", TCon "x", TCon "z" ]) (TCon "y")
+-- | One extra Received
+xc3 = consumesN (TVec [ TCon "y", TCon "x", TCon "z" ])
+      (TVec [ TCon "y", TCon "x", TCon "z", TCon "p" ])
+-- | One extra Required
+xc4 = consumesN
+      (TVec [ TCon "y", TCon "x", TCon "z", TCon "p" ])
+      (TVec [ TCon "y", TCon "x", TCon "z" ])
+xc5 = consumesN (TVec [ TCon "y" ]) (TVec [ TLoc Ho $ TCon "y" ])
+
+-- | The inputs at the locations do interact.
+xc6 = consumesN (TVec [ TLoc Ho $ TCon "y" ]) (TVec [ TLoc Ho $ TCon "y" ])
+
+-- | The inputs at the locations do interact but leave some behind.
+xc7 = consumesN (TVec [ TCon "x", TLoc Ho $ TCon "y" ]) (TVec [ TLoc Ho $ TCon "y" ])
+
+-- | This should saturate, even though the inputs are reversed
+xc8 = consumesN (TVec [ TCon "x", TLoc Ho $ TCon "y" ]) (TVec [ TLoc Ho $ TCon "y", TCon "x" ])
+                            
+consumeN :: T -> T -> Either String T
+consumeN t1@(tIn :=> tOut) t2@(tIn' :=> tOut') = result  
+  where
+   intermediary :: (T,T)
+   intermediary = tIn' `consumesN` tOut
+
+   result :: Either String T
+   result = case intermediary of
+     (TCon "", TCon "") -> Right $ tIn :=> tOut'
+     (TCon "", TVec []) -> Right $ tIn :=> tOut'
+     (TCon "", x)       -> Right $ (tIn <> x) :=> tOut'
+     (TVec [], TCon "") -> Right $ tIn :=> tOut'
+     (TVec [], TVec []) -> Right $ tIn :=> tOut'
+     (TVec [], x)       -> Right $ (tIn <> x) :=> tOut'
+     _ -> Left $ mconcat $
+            [ "error could not merge "
+            , show t1
+            , " "
+            , show t2
+            , " "
+            , show intermediary
+            ]
+
+exCons :: Either String T
+-- | Simple fusion example.
+exCons = (TCon "a" :=> TCon "b") `consumeN` (TCon "b" :=> TCon "c")
+-- | This Time wrapped in a vector.
+exCons' = (TCon "a" :=> TVec[TCon "b"]) `consumeN` (TCon "b" :=> TCon "c")
+-- | Wrapped in two vectors.
+exCons'' = (TCon "a" :=> TVec[TVec[TCon "b"]]) `consumeN` (TCon "b" :=> TCon "c")
+
+exCons2 = (TCon "a" :=> TCon "b") `consumeN` (TCon "" :=> TCon "c")
