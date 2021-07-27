2cfbc41f0cd5c870ff7b6b58def9bf9752bc4903
cstml <cstmlcodes@gmail.com>
2021-07-27 16:48:30 +0100

    new: test file

---
+++ test/Main.hs
@@ -0,0 +1,22 @@
+module Main where
+
+
+import Test.Tasty (defaultMain, testGroup)
+import Test.Tasty.HUnit (assertEqual, testCase)
+
+double x = x * 2
+
+half x = x / 2
+
+main = defaultMain unitTests
+
+unitTests =
+  testGroup
+    "Unit tests"
+    [doublingMakesNumbersBigger, halvingMakesNumbersSmaller]
+
+doublingMakesNumbersBigger =
+  testCase "Double of 4 is 8" $ assertEqual [] 8 (double 4)
+
+halvingMakesNumbersSmaller =
+  testCase "Half of 9 is 4" $ assertEqual [] 4 (half 9)
