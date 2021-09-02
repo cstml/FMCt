247c5ba18a7618d398e669d0479ac537bf735593
cstml <cstmlcodes@gmail.com>
2021-09-02 13:05:40 +0100

    update: tidy up

--- src/FMCt.hs
+++ src/FMCt.hs
@@ -1,11 +1,11 @@
 -- | Module is made just for Re-Exporting basic functions.
 module FMCt (module X, module XX) where
 
-import FMCt.Aux.Pretty as X
-import FMCt.Aux.ToTex as X
-import FMCt.Evaluator as X (eval, eval1, eval1', evalToString)
-import FMCt.Examples as X (examplesList)
-import FMCt.Parsing as X (parseFMC, parseFMCtoString, parseType)
-import FMCt.Pretty as X (printOutput, printStack)
-import FMCt.TypeChecker as X (derive, typeCheck, typeCheckP)
+import FMCt.Aux.Pretty   as X
+import FMCt.Aux.ToTex    as X
+import FMCt.Evaluator    as X (eval, eval1, eval1', evalToString)
+import FMCt.Examples     as X (examplesList)
+import FMCt.Parsing      as X (parseFMC, parseFMCtoString, parseType)
+import FMCt.Pretty       as X (printOutput, printStack)
+import FMCt.TypeChecker  as X (derive, typeCheck, typeCheckP)
 import FMCt.TypeChecker2 as XX (derive2, getTermType, pShow')
