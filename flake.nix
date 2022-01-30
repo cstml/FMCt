2c38d5905fabf8083ed065436607702d41dbc104
cstml <cstmlcodes@gmail.com>
2022-01-30 17:37:49 +0000

    Update:nix

--- flake.nix
+++ flake.nix
@@ -1,67 +1,41 @@
 {
-  description = "haskell-template's description";
+  description = "FMCt Programming Language";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/46821ea01c8f54d2a20f5a503809abfc605269d7";
+    nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
   };
   outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ] (system:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         overlays = [ ];
-        pkgs =
-          import nixpkgs { inherit system overlays; config.allowBroken = true; };
-        # https://github.com/NixOS/nixpkgs/issues/140774#issuecomment-976899227
-        m1MacHsBuildTools =
-          pkgs.haskellPackages.override {
-            overrides = self: super:
-              let
-                workaround140774 = hpkg: with pkgs.haskell.lib;
-                  overrideCabal hpkg (drv: {
-                    enableSeparateBinOutput = false;
-                  });
-              in
-              {
-                ghcid = workaround140774 super.ghcid;
-                ormolu = workaround140774 super.ormolu;
-              };
-          };
-        project = returnShellEnv:
+        
+        pkgs = import nixpkgs {inherit system overlays; config.allowBroken = true;};             
+        additionalPckgs = with pkgs;[
+          nixfmt          
+        ];
+        
+        additionalHaskellPckgs = with pkgs.haskellPackages;[
+          cabal-install
+          ghcid
+          hasktags
+        ];
+        
+        project = returnShellEnv: 
           pkgs.haskellPackages.developPackage {
             inherit returnShellEnv;
-            name = "haskell-template";
+            name = "FMCt";
             root = ./.;
-            withHoogle = false;
-            overrides = self: super: with pkgs.haskell.lib; {
-              # Use callCabal2nix to override Haskell dependencies here
-              # cf. https://tek.brick.do/K3VXJd8mEKO7
-              # Example: 
-              # > NanoID = self.callCabal2nix "NanoID" inputs.NanoID { };
-              # Assumes that you have the 'NanoID' flake input defined.
-            };
-            modifier = drv:
-              pkgs.haskell.lib.addBuildTools drv
-                (with (if system == "aarch64-darwin"
-                then m1MacHsBuildTools
-                else pkgs.haskellPackages); [
-                  # Specify your build/dev dependencies here. 
-                  cabal-fmt
-                  cabal-install
-                  ghcid
-                  haskell-language-server
-                  ormolu
-                  pkgs.nixpkgs-fmt
-                ]);
+            withHoogle = true;                    
+            modifier = drv: pkgs.haskell.lib.addBuildTools drv (
+              additionalHaskellPckgs ++ additionalPckgs);
           };
       in
-      {
-        # Used by `nix build` & `nix run` (prod exe)
-        defaultPackage = project false;
-
-        # Used by `nix develop` (dev shell)
-        devShell = project true;
-      });
+        {
+          # Used by `nix build` & `nix run` (prod exe)
+          defaultPackage = project false;
+          
+          # Used by `nix develop` (dev shell)
+          devShell = project true;
+        });
 }
